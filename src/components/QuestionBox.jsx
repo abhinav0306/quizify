1c8454e4d5c587189d5394bca48a450b5e15b1ba
abhinav0306 <abhinavs0603@gmail.com>
2024-01-19 14:35:36 +0530

    questions updating on click

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -1,29 +1,55 @@
 import React, { useState } from "react";
 import "./css/box.css";
 import questions from "../assets/questions.js";
+
 export default function QuestionBox() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [highlight, setHighlight] = useState(false);
   const current = questions[currentIndex];
-
+  // handleHighligt and HandleRemoveHighlight are two functions that will be called onclick of respective buttons
+  const handleHighlight = () => {
+    setHighlight(true);
+  };
+  const handleRemoveHighlight = () => {
+    setHighlight(false);
+  };
+  // handleClick function will change the question and options whenever an option is clicked
+  const handleClick=()=>{
+    setCurrentIndex((prev)=>prev+1)
+  }
   return (
     // creating a div that will contain question and options
     <div className="container flex">
-      <div className="queBox flex">
+      <div className={`queBox flex ${highlight ? "highlighted" : ""}`}>
         {/* this div will display the current question number */}
+
         <div className="num">
           <h4 className="queNum flex">{`${currentIndex + 1}/${
             questions.length
           }`}</h4>
         </div>
         {/* it will display the question */}
-        <div className="queText flex">{current.text}</div>
+
+        <div className={`queText flex ${highlight ? "highlightedText" : ""}`}>
+          {current.text}
+        </div>
         {/* this block will display the options using map higher order function */}
+
         <div className="options flex">
           {current.options.map((option) => (
-            <button key={option.id} className="optionBtn">
+            <button key={option.id} className="optionBtn" onClick={handleClick}>
               {option.text}
             </button>
           ))}
+        </div>
+        {/* highlight buttons are created here to highligt the question */}
+        <div className="highlight">
+          <button className="highBtn" onClick={handleHighlight}>
+            HIGHLIGHT
+          </button>
+          <button className="highBtn" id="rem" onClick={handleRemoveHighlight}>
+            REMOVE HIGHLIGHT
+          </button>
         </div>
       </div>
     </div>
