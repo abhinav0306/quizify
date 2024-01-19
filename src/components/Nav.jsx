44163fbc8e4b032718da1dcb337dc3df7b065379
abhinav0306 <abhinavs0603@gmail.com>
2024-01-19 15:05:19 +0530

    dark mode button added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./css/nav.css";
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+
 export default function Nav() {
+  const [isDarkMode, setDarkMode] = React.useState(false);
+  const toggleDarkMode = (checked: boolean) => {
+    setDarkMode(checked);
+  };
   return (
     <div className="navbar">
       {/* logo */}
@@ -12,7 +19,14 @@ export default function Nav() {
       </div>
       {/* dark/light mode toggle button */}
       <div className="modebtn">
-        <button>DARK</button>
+      <DarkModeSwitch
+      style={{ marginBottom: '2rem' }}
+      checked={isDarkMode}
+      onChange={toggleDarkMode}
+      size={30}
+      moonColor="black"
+      // sunColor="white"
+    />
       </div>
     </div>
   );
